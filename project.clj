f61fd1449e112c9207e74a716bfa4007ee006c51
Jon Hall <code@jall.me>
2020-06-15 10:27:59 +0100

    Add lein scaffolding

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject tiny-compiler "0.1.0-SNAPSHOT"
+  :description "A tiny compiler"
+  :url "https://github.com/jall/tiny-compiler"
+  :dependencies [[org.clojure/clojure "1.8.0"]])
---
+++ test/tiny_compiler/core_test.clj
@@ -0,0 +1,7 @@
+(ns tiny-compiler.core-test
+  (:require [clojure.test :refer :all]
+            [tiny-compiler.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
